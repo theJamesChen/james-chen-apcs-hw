c3b9fec810a05dddcfdd6b37c8ff03448860f746
James Chen <thejamesschen@gmail.com>
2014-03-19 09:24:42 -0400

    classwrk

--- 08-linkedlist/0318classwork/MyLinkedList.java
+++ 08-linkedlist/0318classwork/MyLinkedList.java
@@ -10,19 +10,40 @@ public class MyLinkedList {
 	//head = tmp;
 	tmp.setNext(head);
 	head=tmp;
-	
-
     }
     
     public String toString() {
-	String s = " ";
-	//can't get next from null
-	//	s = "" + head + head.getNext() + gead,getNext().getNext();
-	while() {
 
+	//can't get next from null
+	Node tmp = head;
+	
+	String s = " "+tmp.getData();
+	//	s = "" + head + head.getNext() + head.getNext().getNext();
+	while(tmp.getNext()!=null) {
+	    tmp = tmp.getNext();
+	    s = s + ",  " + tmp.getData();
+	      
 	}
 	return s;
     }
+    
+    public void add(int i, String s) {
+	int counter = 0;
+	Node tmp = head;
+	if(i = 0)
+	    head.add(s);
+	else {
+	    
+	    while(tmp.getNext()!=null) {
+		if(counter = i) {
+		    
+		}
+		tmp = tmp.get
+	    }
+	}
+	
+    }
+    
     
 
 }
